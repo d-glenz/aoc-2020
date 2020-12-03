b6d9f9edc80ce019ebe02e9c05cda4294e49c088
Dominik Glenz <doglenz@web.de>
2020-12-03 18:02:22 +0100

    minor improvements to python version

--- day3/day3.py
+++ day3/day3.py
@@ -1,22 +1,10 @@
-input1 = """\
-..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#"""
+from typing import List
 
-
-def replicate_many_times(field: str, many=30):
+def replicate_many_times(field: str, many: int=30) -> List[str]:
     rows = field.split('\n')
     return [row*many for row in rows]
 
-def solution1_and_2(input1, y_mult=1, x_mult=3):
+def solution1_and_2(input1: str, y_mult: int=1, x_mult: int=3) -> int:
     rows = len(input1.split('\n'))
     total_field = replicate_many_times(input1, many=rows*3)
 
@@ -35,4 +23,5 @@ with open('input3.real') as day3_input:
     for x,y in [(1,1),(3,1),(5,1),(7,1),(1,2)]:
         solution *= solution1_and_2(field, x_mult=x, y_mult=y)
 
-print(solution)
+print(f"Solution1: {solution1_and_2(field)}")
+print(f"Solution2: {solution}")
